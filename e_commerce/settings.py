a26c201e6d70a41bc2829d6116151b1ec3263192
Aleenaannmathew <aleenamathewinformal@gmail.com>
2025-11-10 12:15:19 +0530

    Add Cloudinary storage integration

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -216,3 +216,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 
+INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'your_cloud_name',
+    'API_KEY': 'your_api_key',
+    'API_SECRET': 'your_api_secret',
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
